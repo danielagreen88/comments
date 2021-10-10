2bd71b9641160845bb6d880f2a998d8a1cf781dd
daniela-green <danielag@bsightftul.co>
2021-10-10 17:56:30 +0300

    CommentList.tsx: add new comments to list

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -4,7 +4,7 @@ import s from './CommentList.module.css';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Comment } from '../Comment/Comment';
 
-const COMMENTS_COUNT = 500;
+const COMMENTS_COUNT = 100;
 const COMMENTS_COUNT_PER_PAGE = 20;
 
 export const CommentList = () => {
@@ -14,8 +14,8 @@ export const CommentList = () => {
 
   useEffect(() => {
     const loadData = async () => {
-      const data = await getComments(limit.start, limit.end);
-      setComments(data);
+      const newComments = await getComments(limit.start, limit.end);
+      setComments(comments => comments.concat(newComments));
     };
     loadData();
   }, [limit.start, limit.end]);
@@ -24,14 +24,14 @@ export const CommentList = () => {
     if (comments.length >= COMMENTS_COUNT) {
       setHasMore(false);
       return;
-    } else {
-      setLimit(limit => {
-        return {
-          start: limit.start + COMMENTS_COUNT_PER_PAGE,
-          end: limit.end + COMMENTS_COUNT_PER_PAGE,
-        };
-      });
     }
+
+    setLimit(limit => {
+      return {
+        start: limit.start + COMMENTS_COUNT_PER_PAGE,
+        end: limit.end + COMMENTS_COUNT_PER_PAGE,
+      };
+    });
   };
 
   const getEndMessage = () => {
@@ -51,7 +51,7 @@ export const CommentList = () => {
           next={fetchMoreData}
           hasMore={hasMore}
           loader={getLoader()}
-          height={500}
+          height={800}
           endMessage={getEndMessage()}
         >
           {comments.map(comment => (
