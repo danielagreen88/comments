b5fab46b4bbef6ef9a8dba1ce5bab1760e7fc516
daniela-green <danielag@bsightftul.co>
2021-10-10 16:50:58 +0300

    CommentList.tsx: added infinite scroll

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,34 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { getComments } from '../../services/api';
 import s from './CommentList.module.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export const CommentList = () => {
   const [limit, setLimit] = useState({ start: 0, end: 20 });
   const [comments, setComments] = useState<any>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     const loadData = async () => {
-      setIsLoading(true);
       const data = await getComments(limit.start, limit.end);
       setComments(data);
-      setIsLoading(false);
     };
     loadData();
-  }, []);
+  }, [limit.start, limit.end]);
+
+  const fetchMoreData = () => {
+    if (comments.length >= 500) {
+      setHasMore(false);
+    } else {
+      setLimit(limit => {
+        return {
+          start: limit.start + 20,
+          end: limit.end + 20,
+        };
+      });
+    }
+  };
 
   return (
     <div className={s.commentList}>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          Comment List
-          {comments.map((comment: any) => {
-            return <div className={s.comment}>{comment.body}</div>;
-          })}
-        </div>
-      )}
+      <div>
+        Comment List
+        <InfiniteScroll
+          dataLength={comments.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          height={500}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {comments.map((comment, index) => (
+            <div className={s.comment} key={index}>
+              <div>{index}</div>
+              {comment.body}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 };
