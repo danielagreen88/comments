0d1df575854af7056d6a1f681b8be2adf2bb40ca
daniela-green <danielag@bsightftul.co>
2021-10-10 17:55:52 +0300

    api: fix api to get comments

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function getComments(start: number, end: number) {
   return axios
     .get(
-      `https://jsonplaceholder.typicode.com/comments?_start=${start}&_limit=${end}`,
+      `https://jsonplaceholder.typicode.com/comments?_start=${start}&_end=${end}`,
     )
     .then(res => res.data);
 }
