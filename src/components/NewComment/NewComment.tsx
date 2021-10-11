e10caa1bab8d96a03494bc1a93b8a81e5374bab9
daniela-green <danielag@bsightftul.co>
2021-10-11 21:18:30 +0300

    NewComment.tsx: added new comment button

--- src/components/NewComment/NewComment.tsx
+++ src/components/NewComment/NewComment.tsx
@@ -3,10 +3,15 @@ import s from './NewComment.module.css';
 
 export const NewComment = () => {
   return (
-    <>
+    <div>
       <div className={s.newComment} placeholder={'Write a comment...'}>
         <div contentEditable={true} />
       </div>
-    </>
+      <div className={s.addCommentWrapper}>
+        <button className={s.addComment} onClick={() => {}}>
+          Post
+        </button>
+      </div>
+    </div>
   );
 };
